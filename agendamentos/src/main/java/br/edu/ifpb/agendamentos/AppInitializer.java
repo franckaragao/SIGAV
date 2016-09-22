0d87f43f79bab63d6f69a97eef8937f98a3d0e85
Franck Aragão <fjunior.aragao@gmail.com>
2016-09-21 23:54:06 -0300

    init

---
+++ agendamentos/src/main/java/br/edu/ifpb/agendamentos/AppInitializer.java
@@ -0,0 +1,22 @@
+package br.edu.ifpb.agendamentos;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return null;
+	}
+
+}
