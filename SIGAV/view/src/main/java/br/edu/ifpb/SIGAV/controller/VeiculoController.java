bef016b0d8f1eae7301c916bd3e48b045e5096fb
Franck Aragão <fjunior.aragao@gmail.com>
2016-09-22 02:06:46 -0300

    thymeleaf configurado

--- SIGAV/view/src/main/java/br/edu/ifpb/SIGAV/controller/VeiculoController.java
+++ SIGAV/view/src/main/java/br/edu/ifpb/SIGAV/controller/VeiculoController.java
@@ -1,8 +1,18 @@
 package br.edu.ifpb.SIGAV.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 
+ * @author <a href="https://github.com/FranckAJ">Franck Aragão</a>
+ *
+ */
 @Controller
 public class VeiculoController {
-
+	
+	@RequestMapping("/veiculos/novo")
+	public String novo(){
+		return "veiculo/cadastro_veiculo";
+	}
 }
