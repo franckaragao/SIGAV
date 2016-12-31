f5f7652e3feac12b649ff240aa05b3df9c187ad1
Franck Aragão <fjunior.aragao@gmail.com>
2016-12-31 02:25:13 -0300

    fazendo ordenação das colunas

--- SIGAV/view/src/main/java/br/edu/ifpb/SIGAV/controller/page/PageWrapper.java
+++ SIGAV/view/src/main/java/br/edu/ifpb/SIGAV/controller/page/PageWrapper.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -42,6 +44,10 @@ public class PageWrapper<T> {
 	public int getSize() {
 		return page.getSize();
 	}
+	
+	public boolean isEmpty() {
+		return page.getContent().isEmpty();
+	}
 
 	public boolean isFirst() {
 		return page.isFirst();
@@ -66,5 +72,58 @@ public class PageWrapper<T> {
 	 */
 	public String urlTo(int page) {
 		return uriBuilder.replaceQueryParam("page", page).build(true).encode().toUriString();
+	}
+	
+	/**
+	 * 
+	 * @param property
+	 * @return
+	 */
+	public String urlSortBy(String property){
+		UriComponentsBuilder uriBuildOrder = UriComponentsBuilder.
+				fromUriString(uriBuilder.build(true).encode().toUriString());
+		
+		String valueSort = String.format("%s,%s", property, invertSort(property));
+		
+		return uriBuildOrder.replaceQueryParam("sort", valueSort).build(true).encode().toUriString();
+	}
+	
+	/**
+	 * 
+	 * @param property
+	 * @return
+	 */
+	public boolean isDescendent(String property){
+		return invertSort(property).equals("asc");
+	}
+	
+	/**
+	 * 
+	 * @param property
+	 * @return
+	 */
+	public boolean isOrder(String property){
+		
+		if(page.getSort() == null)
+			return false;
+		
+		return page.getSort().getOrderFor(property) != null ? true:false;
+	}
+	
+	/**
+	 * 
+	 * @param property
+	 * @return
+	 */
+	private String invertSort(String property){
+		String direction = "asc";
+		
+		Order order = page.getSort() != null ? page.getSort().getOrderFor(property) : null;
+		
+		if(order != null){
+			direction = Sort.Direction.ASC.equals(order.getDirection()) ? "desc" : "asc";
+		}
+		
+		return direction;
 	}
 }
